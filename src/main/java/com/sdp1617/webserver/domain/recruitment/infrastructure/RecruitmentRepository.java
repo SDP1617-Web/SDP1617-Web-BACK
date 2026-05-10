ef576da11758eddf152a17c3d6ce3ab0c917d684
hyunbin <s9476721@naver.com>
2026-05-11 00:33:37 +0900

    feat: 활성 모집 공고 및 질문 목록 조회 서비스 구현 (#9)

--- src/main/java/com/sdp1617/webserver/domain/recruitment/infrastructure/RecruitmentRepository.java
+++ src/main/java/com/sdp1617/webserver/domain/recruitment/infrastructure/RecruitmentRepository.java
@@ -3,5 +3,8 @@ package com.sdp1617.webserver.domain.recruitment.infrastructure;
 import com.sdp1617.webserver.domain.recruitment.entity.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    Optional<Recruitment> findFirstByIsActiveTrue();
 }
